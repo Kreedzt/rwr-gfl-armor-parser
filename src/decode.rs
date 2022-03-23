3120079e14b8adb2247412ec6fcfec10963de978
Kreedzt <zhaozisong1@live.com>
2022-03-23 09:20:59 +0800

    🐛  (fixed decode)

--- src/decode.rs
+++ src/decode.rs
@@ -1,13 +1,15 @@
 use std::fs::File;
+use std::io::Read;
 use encoding_rs::*;
+
 
 pub fn read_file_decode_to_utf8(path: &str) -> Result<String, String> {
     let file = File::open(path);
 
     match file {
-        Ok(_) => {
-            let buf_string = String::new();
-            // let bytes = result.read_to_string(&mut buf_string).unwrap();
+        Ok(mut result) => {
+            let mut buf_string = String::new();
+            result.read_to_string(&mut buf_string).unwrap();
 
             let (cow, _) = UTF_8.decode_with_bom_removal(buf_string.as_bytes());
 
